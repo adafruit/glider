9842d23523cd123466ee0d2c72a12cd3496bd062
Lincoln Roth <lincolnmroth@gmail.com>
2020-08-06 22:49:05 -0400

    initial reading test

--- App.js
+++ App.js
@@ -19,39 +19,11 @@ import {
 } from 'react-native';
 
 import BleManager from 'react-native-ble-manager';
+import loadLocalResource from 'react-native-local-resource'
+import stubbedCode from './stubbed.py'
 const BleManagerModule = NativeModules.BleManager;
 const bleManagerEmitter = new NativeEventEmitter(BleManagerModule);
 
-
-function peripheralReducer(state, action) {
-    if (action.action == "add") {
-        let peripheral = action.peripheral;
-        console.log(peripheral);
-        if (state.has(peripheral.id)) {
-            return state; // No state change
-        }
-        if (!peripheral.advertising.serviceUUIDs || peripheral.advertising.serviceUUIDs.length == 0 || peripheral.advertising.serviceUUIDs[0].toLowerCase() != 'adaf0100-4369-7263-7569-74507974686e') {
-            return state;
-        }
-        console.log(peripheral);
-        var newMap = new Map(state);
-        newMap.set(peripheral.id, peripheral);
-        return newMap;
-    } else if (action.action == "scan") {
-        BleManager.scan([], 3).then((results) => {
-            console.log('Scanning...');
-        });
-    } else if (action.action == "clear") {
-        state.clear();
-    }
-    return state;
-}
-
-const service = 'adaf0100-4369-7263-7569-74507974686e';
-const contentsCharacteristic = 'adaf0201-4369-7263-7569-74507974686e';
-const filenameCharacteristic = 'adaf0200-4369-7263-7569-74507974686e';
-const versionCharacteristic = 'adaf0203-4369-7263-7569-74507974686e';
-const lengthCharacteristic = 'adaf0202-4369-7263-7569-74507974686e';
 
 function codeReducer(state, action) {
   let newState = {code: state.code, peripheral_id: state.peripheral_id, queue: state.queue, version: state.version + 1};
@@ -103,219 +75,16 @@ function codeReducer(state, action) {
   return newState;
 }
 
+function stubber () {
+  loadLocalResource(stubbedCode).then((stubbedCodeContent) => {
+          console.log("stubbed was loaded: " + stubbedCodeContent)
+          return stubbedCodeContent
+      }
+  )
+}
 export default function App() {
     const scheme = useColorScheme();
-    const currentAppState = useAppState();
-    const [bleState, setBleState] = useState("stopped");
-    const [peripherals, changePeripherals] = useReducer(peripheralReducer, new Map());
-    const [peripheral, setPeripheral] = useState(null);
-    const [fileState, setFileState] = useState("unloaded");
-    const [fileLength, setFileLength] = useState(-1);
-
     const [code, changeCode] = useReducer(codeReducer, {code:"", version:0, peripheral_id: null});
-
-    useEffect(() => {
-        if (currentAppState === 'active') {
-          console.log('App has come to the foreground!', bleState);
-          BleManager.start({showAlert: true});
-          BleManager.checkState();
-        } else {
-            if (peripheral) {
-              changeCode({"type": "disconnect", "peripheral_id": peripheral.id});
-              BleManager.disconnect(peripheral.id);
-            }
-
-            setBleState("disconnected");
-        }
-    }, [currentAppState]);
-
-    const handleUpdateValueForCharacteristic = (data) => {
-        console.log(data);
-        console.log('Received data from ' + data.peripheral + ' characteristic ' + data.characteristic, bytesToString(data.value));
-        changeCode({"type": "read", "data": bytesToString(data.value)});
-    }
-
-    const handleUpdateState = (data) => {
-      console.log("update state", data);
-      if (data.state == "on") {
-        console.log("started");
-        setBleState("started");
-      }
-    }
-
-    const handleStopScan = () => {
-        console.log('Scan is stopped');
-        setBleState('selectPeripheral');
-    }
-
-    const handleDiscoverPeripheral = (peripheral) => {
-        console.log('Got ble peripheral', peripheral);
-        peripheral.connected = false;
-        changePeripherals({"action": "add", "peripheral": peripheral});
-    }
-
-    const handleDisconnectedPeripheral = (data) => {
-        // let peripherals = this.state.peripherals;
-        // let peripheral = peripherals.get(data.peripheral);
-        // if (peripheral) {
-        //   peripheral.connected = false;
-        //   peripherals.set(peripheral.id, peripheral);
-        //   this.setState({peripherals});
-        // }
-        setBleState("disconnected");
-        
-        Alert.alert(
-          "Device Disconnected",
-          "You have lost connection with your device",
-          [
-            {
-              text: "Dismiss",
-            }
-          ],
-          { cancelable: false }
-        );
-        console.log('Disconnected from ' + data.peripheral);
-      }
-
-    function handleConnectPeripheral() {
-      console.log("handle connect to", peripheral);
-      BleManager.connect(peripheral.id).then(() => {
-        setBleState("connected");
-        changeCode({"type": "connect", "peripheral_id": peripheral.id});
-        setTimeout(() => {
-
-          BleManager.retrieveServices(peripheral.id).then((peripheralInfo) => {
-            console.log(peripheralInfo);
-
-            setTimeout(() => {
-              BleManager.startNotification(peripheral.id, service, contentsCharacteristic).then(() => {
-                console.log('Started notification on ' + peripheral.id);
-                setTimeout(() => {
-                  BleManager.write(peripheral.id, service, filenameCharacteristic, stringToBytes("/code.py")).then(() => {
-                    console.log('Wrote filename');
-                    setFileState("nameSet");
-                  });
-
-                }, 500);
-              }).catch((error) => {
-                console.log('Notification error', error);
-              });
-            }, 200);
-          });
-
-        }, 900);
-      }).catch((error) => {
-        setBleState("disconnected");
-        console.log('Connection error', error);
-      });
-    }
-
-    useEffect(() => {
-        if (fileState == "nameSet") {
-          // Load the file length and then load it all.
-          setTimeout(() => {
-            BleManager.read(peripheral.id, service, lengthCharacteristic).then((readData) => {
-              let length = readData[0] + (readData[1] << 8) + (readData[2] << 16) + (readData[3] << 24);
-              console.log('fileLength', readData, length);
-              changeCode({"type": "clear"});
-              setFileLength(length);
-              setFileState("loading");
-            });
-
-          }, 500);
-        } else if (fileState == "loading") {
-          let command = new Array(4);
-          command.fill(0);
-          command[0] = 4;
-          command[2] = 1;
-          setTimeout(() => {
-            BleManager.write(peripheral.id, service, contentsCharacteristic, command)
-                      .catch((error) => {
-                        console.log('Command error', error);
-                      });
-          }, 200);
-        }
-    }, [fileState]);
-
-    useEffect(() => {
-      console.log("loaded", code.code.length, fileLength);
-      if (fileState == "loading" && code.code.length == fileLength) {
-        setFileState("loaded");
-      }
-    }, [code, fileState, fileLength]);
-
-    useEffect(() => {
-        if (peripheral != null && bleState != "connected") {
-            handleConnectPeripheral();
-        }
-    }, [peripheral, bleState]);
-
-    useEffect(() => {
-        console.log("new blestate", bleState);
-        if (bleState == "permOk") {
-        } else if (bleState == "started") {
-            changePeripherals({"action": "clear"});
-            BleManager.getConnectedPeripherals([]).then((peripheralsArray) => {
-              for (p of peripheralsArray) {
-                console.log(p);
-                p.connected = true;
-                BleManager.connect(p.id).then(() => {
-                BleManager.retrieveServices(p.id).then((peripheralInfo) => {
-                  console.log(peripheralInfo);
-                  p.advertising.serviceUUIDs = [peripheralInfo.services[2].uuid];
-                  changePeripherals({"action": "add", "peripheral": p});
-                });
-              });
-              }
-              console.log('Connected peripherals: ' + peripheralsArray.length);
-            });
-            changePeripherals({"action": "scan"});
-        } else if (bleState == "disconnected") {
-            // set a timeout and try to reconnect
-        } else if (bleState == "selectPeripheral") {
-            console.log(peripherals, peripherals.values());
-            if (peripherals.size == 1) {
-                let peripheral = [...peripherals][0][1];
-                console.log("selecting", peripheral);
-                setPeripheral(peripheral);
-            }
-          }
-      }, [bleState]);
-
-    useEffect(() => {
-        const handlerDiscover = bleManagerEmitter.addListener('BleManagerDiscoverPeripheral', handleDiscoverPeripheral );
-        const handlerStop = bleManagerEmitter.addListener('BleManagerStopScan', handleStopScan );
-        const handlerDisconnect = bleManagerEmitter.addListener('BleManagerDisconnectPeripheral', handleDisconnectedPeripheral );
-        const handlerUpdate = bleManagerEmitter.addListener('BleManagerDidUpdateValueForCharacteristic', handleUpdateValueForCharacteristic );
-        const handlerUpdateState = bleManagerEmitter.addListener('BleManagerDidUpdateState', handleUpdateState);
-
-        if (Platform.OS === 'android' && Platform.Version >= 23) {
-            setBleState("permCheck");
-            PermissionsAndroid.check(PermissionsAndroid.PERMISSIONS.ACCESS_COARSE_LOCATION).then((result) => {
-                if (result) {
-                  setBleState("permOk");
-                } else {
-                  PermissionsAndroid.requestPermission(PermissionsAndroid.PERMISSIONS.ACCESS_COARSE_LOCATION).then((result) => {
-                    if (result) {
-                      setBleState("permOk");
-                    } else {
-                      setBleState("permNak");
-                    }
-                  });
-                }
-          });
-        } else {
-            setBleState("permOk");
-        }
-
-        return () => {
-            handlerDiscover.remove();
-            handlerStop.remove();
-            handlerDisconnect.remove();
-            handlerUpdate.remove();
-            handlerUpdateState.remove();
-        };
-    }, []);
 
     return (
       <AppearanceProvider><NavigationContainer theme={scheme === 'dark' ? DarkTheme : DefaultTheme}>
@@ -326,23 +95,22 @@ export default function App() {
         initialDrawerSize={17}
         renderContainerView={() => (
         <View>
-            <StatusSummary bleState={bleState} />
-            <CodeEditor 
-              code={code.code} 
-              changeCode={changeCode} 
-              fileState={fileState} 
-              fileName="/code.py" 
-              fileVersion={code.version}
-            /> 
+            <StatusSummary bleState={"stubbed"} />
+            <CodeEditor
+              code={await stubber()}
+              changeCode={changeCode}
+              fileState="loaded"
+              fileName="/code.py"
+              fileVersion={"1"}
+            />
         </View>)}
-        renderDrawerView={() => (<Status bleState={bleState} peripherals={peripherals} setPeripheral={setPeripheral} />)}
-        renderInitDrawerView={() => (<StatusSummary bleState={bleState}/>)}
-        
+        renderDrawerView={() => (<Status bleState={"stubbed"} />)}
+        renderInitDrawerView={() => (<StatusSummary bleState={"stubbed"}/>)}
       />
       </SafeAreaView>
       </NavigationContainer></AppearanceProvider>
     );
 
-}
-       
 
+
+}
--- metro.config.js
+++ metro.config.js
@@ -14,4 +14,7 @@ module.exports = {
       },
     }),
   },
+  resolver: {
+    assetExts: ["py"]
+  }
 };
